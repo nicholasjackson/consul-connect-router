912386337e58d8e3ce87becd1956ec529f6817d6
Nic Jackson <jackson.nic@gmail.com>
2018-07-14 05:50:23 +0000

    Addd Connect SDK from branch, cleanup logic for server termination

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"io"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/consul/connect"
@@ -54,7 +59,25 @@ func main() {
 	httpClient = svc.HTTPClient()
 	http.HandleFunc("/", handler)
 
-	http.ListenAndServe(":8181", nil)
+	// setup the server
+	s := http.Server{}
+	s.Addr = ":8181"
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		logger.Info("Received termination signal, shutting down", "signal", sig)
+
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		s.Shutdown(ctx)
+	}()
+
+	err = s.ListenAndServe()
+	if err != nil {
+		logger.Error("Error shutting down server", "error", err)
+	}
 }
 
 func handler(rw http.ResponseWriter, r *http.Request) {
@@ -68,6 +91,7 @@ func handler(rw http.ResponseWriter, r *http.Request) {
 
 	resp, err := httpClient.Get("https://" + us.Host + ".service.consul")
 	if err != nil {
+		logger.Error("Unable to contact upstream", "error", err)
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
